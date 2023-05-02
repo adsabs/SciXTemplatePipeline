d041e2e14ce746a5a2f5c7b133384d691bbd6ebd
tjacovich <tjacovich@cfa.harvard.edu>
2023-05-02 14:00:03 -0400

    removed postgres_init script.

--- postgres_init_gRPC_status.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#docker stop postgres
-#docker rm postgres
-#docker run -d -e POSTGRES_USER="root" -e POSTGRES_PASSWORD="root" -p 5432:5432 --name postgres  postgres:12.6 #http://localhost:15672
-#sleep 30
-docker exec -it postgres bash -c "psql -c \"CREATE ROLE TEMPLATE WITH LOGIN PASSWORD 'TEMPLATE';\""
-docker exec -it postgres bash -c "psql -c \"CREATE DATABASE TEMPLATE;\""
-docker exec -it postgres bash -c "psql -c \"GRANT CREATE ON DATABASE TEMPLATE TO TEMPLATE;\""
-docker exec -it postgres bash -c "psql -c \"GRANT CREATE ON DATABASE TEMPLATE TO TEMPLATE;\""
