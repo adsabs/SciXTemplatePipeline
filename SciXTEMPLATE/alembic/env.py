e25c723eb5c1d708c12e2fb8228e3904601abb77
tjacovich <tjacovich@cfa.harvard.edu>
2023-05-18 14:27:45 -0400

    Refactored alembic env.py

--- SciXTEMPLATE/alembic/env.py
+++ SciXTEMPLATE/alembic/env.py
@@ -3,10 +3,10 @@ import sys
 from logging.config import fileConfig
 
 from alembic import context
+from SciXPipelineUtils.utils import load_config
 from sqlalchemy import engine_from_config, pool
 
 import TEMPLATE.models as models
-from TEMPLATE.utils import load_config
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
